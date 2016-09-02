2b4e0e86ca6bf692ab7e0c791f7db9e43e82fcff
iparker3964 <iparker3964@yahoo.com>
2016-09-02 08:33:02 -0500

    version 2

---
+++ src/ClassicPizza.java
@@ -0,0 +1,43 @@
+
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Chelsea
+ */
+public class ClassicPizza {
+    public enum ClassicTypes {ComboClassic, VeggieLover, MeatLover, SouthWestern, Hawaiian, ChickenAlfredo };
+    public enum CrustSize{CHEESESTUFF, THIN, NORMAL};
+    private double pizzaPrice = 0;
+    private double CrustPrice = 0;
+    private ClassicTypes myClassicTypes;
+    private CrustSize myCrustSize;
+    ArrayList<ClassicPizza> ClassicPizzas = new ArrayList<ClassicPizza>();
+    public void 
+    
+    
+    ClassicPizza(ClassicTypes pizza, CrustSize crust){
+                pizza = myClassicTypes;
+                if(myCrustSize == CrustSize.CHEESESTUFF){
+                    CrustPrice = 1.00;
+                }
+                else if(myCrustSize == CrustSize.THIN){
+                    CrustPrice = 00.50;
+                }
+                else{
+                    CrustPrice = 00.25;
+                
+                }
+                if(myClassicTypes == ClassicTypes.ComboClassic){
+                   
+                }
+    
+    
+    }
+}

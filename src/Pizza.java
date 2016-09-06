d023735ab943425ad11d80003955e57e70f7f6c2
Trey50Daniel <tagg8945@gmail.com>
2016-09-06 11:21:38 -0500

    Made changes to Pizza object with multiple constructors

--- src/Pizza.java
+++ src/Pizza.java
@@ -1,3 +1,6 @@
+
+import java.util.ArrayList;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +12,34 @@
  * @author tagg8
  */
 public class Pizza {
+    public enum ClassicTypes {ComboClassic, VeggieLover, MeatLover, SouthWestern, Hawaiian, ChickenAlfredo };
+    public enum CrustType{CHEESESTUFF, THIN, NORMAL};
+    private double pizzaPrice = 0;
+    private double CrustPrice = 0;
+    private ClassicTypes myClassicTypes;
+    private CrustType myCrustSize;
+    ArrayList<Pizza> ClassicPizzas = new ArrayList<Pizza>();
     
+    public Pizza() {
+        
+    }
+    
+    public Pizza(ClassicTypes pizza, CrustType crust){
+                pizza = myClassicTypes;
+                if(myCrustSize == CrustType.CHEESESTUFF){
+                    CrustPrice = 1.00;
+                }
+                else if(myCrustSize == CrustType.THIN){
+                    CrustPrice = 00.50;
+                }
+                else{
+                    CrustPrice = 00.25;
+                
+                }
+                if(myClassicTypes == ClassicTypes.ComboClassic){
+                   
+                }
+    
+    
+    }
 }

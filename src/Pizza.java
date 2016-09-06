2d1dd3c88ec9209365776998ac57c66b535cc0bf
iparker3964 <iparker3964@yahoo.com>
2016-09-06 14:17:56 -0500

    Merge origin/master
    Conflicts:
    	src/Pizza.java

--- src/Pizza.java
+++ src/Pizza.java
@@ -11,12 +11,16 @@ import java.util.ArrayList;
  *
  * @author tagg8
  */
-public class Pizza {
-    public enum ClassicTypes {PEPPERONI, SAUSAGE, CHEESE, MUSHROOM };
-    public enum SpecialtyTypes {ComboClassic, VeggieLover, MeatLover, SouthWestern, Hawaiian, ChickenAlfredo};
+public final class Pizza {
+    public enum ClassicTypes {PEPPERONI, SAUSAGE, CHEESE, MUSHROOM, NONE };
+    private ClassicTypes myClassicTypes;
+    public enum SpecialtyTypes {COMBOCLASSIC, VEGGIELOVER, MEATLOVER, SOUTHWESTERN, HAWAIIAN, CHICKENALFREDO, NONE};
+    private SpecialtyTypes mySpecialty;
     public enum CrustType {CHEESESTUFF, THIN, NORMAL};
+    public enum pizzaSize {SMALL, MEDIUM, LARGE, XLARGE, NONE};
     public enum Toppings {PEPPERONI, CHEESE, SAUSAGE, MUSHROOM, ANCHOVIES, ONION, PEPPER, BEEF, BBQ };
     private boolean isCustom;
+    
     private double pizzaPrice = 0;
     private double CrustPrice = 0;
     
@@ -25,12 +29,56 @@ public class Pizza {
         isCustom = false;
     }
     
+    public Pizza(String pizza, String pizzaSize, String crust){
+        switch (pizza){
+                case "PEPPERONI":
+                     myClassicTypes = ClassicTypes.PEPPERONI;
+                     getPizza(pizza);
+                    break;
+                case "SAUSAGE":
+                    myClassicTypes = ClassicTypes.SAUSAGE;
+                    break;
+                case "CHEESE":
+                    myClassicTypes = ClassicTypes.CHEESE;
+                    break;
+                case "MUSHROOM":
+                    myClassicTypes = ClassicTypes.MUSHROOM;
+                   break;
+                case "COMBOCLASSIC":
+                     mySpecialty = SpecialtyTypes.COMBOCLASSIC;
+                     getPizza(pizza);
+                    break;
+                case "VEGGIELOVER":
+                    myClassicTypes = ClassicTypes.SAUSAGE;
+                    break;
+                case "MEATLOVER":
+                    myClassicTypes = ClassicTypes.CHEESE;
+                    break;
+                case "SOUTHWESTERN":
+                    myClassicTypes = ClassicTypes.MUSHROOM;
+                   break;
+                 case "HAWAIIAN":
+                     myClassicTypes = ClassicTypes.PEPPERONI;
+                     getPizza(pizza);
+                    break;
+                case "CHICKENALFREDO":
+                    myClassicTypes = ClassicTypes.SAUSAGE;
+                    break;
+                
+                default:
+                     myClassicTypes = ClassicTypes.NONE;
+                    break;
+            }
+    }
     public Pizza(String pizza, String crust){
         
     }
     
     public void addTopping() {
         
+    }
+    public String getPizza(String pizza){
+       return pizza;
     }
 }
 

7495cc15243254d8015e8cafcf89c430a0918144
Trey50Daniel <tagg8945@gmail.com>
2016-09-07 19:06:26 -0500

    Updated Beverage Class

--- src/Beverage.java
+++ src/Beverage.java
@@ -13,37 +13,49 @@ import java.util.ArrayList;
  */
 public class Beverage {
     public enum size {SMALL, MEDIUM, LARGE, XLARGE};
-    public enum drinkTypes {COKE, PEPSI, SPRIT, DRPEPPER};
+    public enum drinkTypes {COKE, PEPSI, SPRITE, DRPEPPER};
     private double drinkPrice = 0;
     private size mySize;
     private drinkTypes myDrinkType;
     
-    public Beverage(String bevSize, String drink){
-           //  bevSize = mySize;
-            switch (bevSize){
-                case "LARGE":
-                    mySize = size.LARGE;
-                    break;
-                case "MEDIUM":
-                    mySize = size.MEDIUM;
-                    break;
-                case "":
-            }
+    public Beverage(){
+        
+    }
+    
+    public void setDrinkType(String drinkType) {
+        switch (drinkType){
+            case "Coke":
+                myDrinkType = drinkTypes.COKE;
+                break;
+            case "Dr. Pepper":
+                myDrinkType = drinkTypes.DRPEPPER;
+                break;
+            case "Pepsi":
+                myDrinkType = drinkTypes.PEPSI;
+                break;
+            case "Sprite":
+                myDrinkType = drinkTypes.SPRITE;
+                break;
+        }
     }
     
     public void setBevSize(String bevSize) {
         switch (bevSize){
             case "XLARGE":
                 mySize = size.XLARGE;
+                drinkPrice = 4.0;
                 break;
             case "LARGE":
                 mySize = size.LARGE;
+                drinkPrice = 3.25;
                 break;
             case "MEDIUM":
                 mySize = size.MEDIUM;
+                drinkPrice = 2.25;
                 break;
             case "SMALL":
                 mySize = size.SMALL;
+                drinkPrice = 1.75;
                 break;
         }
     }
